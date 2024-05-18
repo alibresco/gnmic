f83abb1a25ecacd35b40f1191d7ecd040f0f76ae
Adam Libresco <alibresco@gmail.com>
2024-05-18 00:23:29 -0400

    case

--- pkg/outputs/prometheus_output/prometheus_write_output/prometheus_write_output_test.go
+++ pkg/outputs/prometheus_output/prometheus_write_output/prometheus_write_output_test.go
@@ -9,6 +9,36 @@ import (
 	"github.com/openconfig/gnmic/pkg/outputs"
 )
 
+// 10000 buffer, 1 writer
+// BenchmarkRemoteWrite-10          1000000              1389 ns/op            1977 B/op         37 allocs/op
+
+// 1000 buffer, 1 writer
+// BenchmarkRemoteWrite-10           783300              1523 ns/op            1992 B/op         37 allocs/op
+
+// 256 buffer, 1 writer
+// BenchmarkRemoteWrite-10           842360              1355 ns/op            1993 B/op         37 allocs/op
+
+// 1 buffer, 1 writer
+// BenchmarkRemoteWrite-10           427292              2632 ns/op            1994 B/op         38 allocs/op
+
+// 0 buffer, 1 writer
+// BenchmarkRemoteWrite-10           494935              2125 ns/op            1994 B/op         38 allocs/op
+
+// 10000 buffer, 60 writer
+// BenchmarkRemoteWrite-10          1000000              2757 ns/op            1978 B/op         37 allocs/op
+
+// 1000 buffer, 60 writer
+// BenchmarkRemoteWrite-10           418759              2451 ns/op            1991 B/op         37 allocs/op
+
+// 256 buffer, 60 writer
+// BenchmarkRemoteWrite-10           387818              3391 ns/op            1993 B/op         37 allocs/op
+
+// 1 buffer, 60 writer
+// BenchmarkRemoteWrite-10           380790              3164 ns/op            1993 B/op         38 allocs/op
+
+// 0 buffer, 60 writer
+// BenchmarkRemoteWrite-10           281700              3637 ns/op            1996 B/op         38 allocs/op
+
 func BenchmarkRemoteWrite(b *testing.B) {
 	cfg := map[string]interface{}{
 		"url":                       "http://example.com",
